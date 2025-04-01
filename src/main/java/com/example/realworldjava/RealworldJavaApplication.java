61521c5650b5b2a0950f2126f5ebda4c49e4b2b4
nguyen.trong.hieu <nguyen.trong.hieu@sun-asterisk.com>
2025-04-01 13:19:42 +0700

    Initial project

---
+++ src/main/java/com/example/realworldjava/RealworldJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.realworldjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realworldjava/RealworldJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realworldjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
